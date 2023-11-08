28ec9b502da68560b47f2f467ea540543f9f8c5c
当时只道是寻常 <1207588603@qq.com>
2023-11-08 15:32:01 +0800

    feat：员工管理-员工列表结构

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -22,7 +22,39 @@
         <el-button plain size="small" icon="el-icon-download" type="warning">导出</el-button>
       </el-row>
       <!-- 表格组件 -->
-      <!-- 分页 -->
+
+      <el-table>
+        <el-table-column align="center" label="头像" />
+        <el-table-column label="姓名" />
+        <el-table-column label="手机号" sortable />
+        <el-table-column label="工号" sortable />
+        <el-table-column label="聘用形式" />
+        <el-table-column label="部门" />
+        <el-table-column label="入职时间" sortable />
+        <el-table-column label="操作" width="280px">
+          <template>
+            <el-link type="primary" icon="el-icon-edit">查看</el-link>
+            <el-link type="warning" icon="el-icon-edit">角色</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-popconfirm :title="`是否确认删除角色 `">
+              <el-link type="danger" slot="reference" icon="el-icon-delete">删除</el-link>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <div class="flex justify-end items-center mt-16">
+        <el-pagination
+          background
+          :layout="layout"
+          :page-sizes="pageSizeList"
+          :total="total"
+          :current-page="pageNo"
+          :page-size="pageSize"
+          @current-change="changePageNo"
+          @size-change="changePageSize"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -38,6 +70,11 @@ export default {
     return {
       depts: [],
       defaultProps: { children: 'children', label: 'name' },
+      pageNo: 1, // 当前页数
+      pageSize: 10, // 每页显示条目个数
+      total: 0, // 总条目数
+      pageSizeList: [10, 20, 30, 40, 50],
+      layout: 'total, sizes, prev, pager, next, jumper', // 组件布局，子组件名用逗号分隔
       // 部门名称
       deptName: undefined,
       // 查询参数
@@ -70,6 +107,9 @@ export default {
       if (!value) return true
       return data.name.includes(value)
     },
+
+    changePageNo() {},
+    changePageSize() {},
   },
   watch: {
     /** 根据名称筛选部门树 */
