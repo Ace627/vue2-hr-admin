9b566b459cfca8d90d26cb8537a9278f8aa90545
当时只道是寻常 <1207588603@qq.com>
2023-11-08 11:29:59 +0800

    perf：角色管理 页面结构

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="app-content">
-    <div>
+    <el-card class="mb-16">
       <el-button size="mini" type="primary" icon="el-icon-plus" plain>新增</el-button>
-    </div>
+    </el-card>
 
-    <el-table :data="list">
-      <el-table-column align="center" width="200" label="角色编号" prop="id" />
-      <el-table-column align="center" width="200" label="角色名称" prop="name" />
-      <el-table-column align="center" width="200" label="状态">
-        <template v-slot="{ row }">
-          <span> {{ row.state === 1 ? '已启用' : row.state === 0 ? '未启用' : '无' }} </span>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" label="描述" prop="description" />
-      <el-table-column align="center" label="操作" />
-    </el-table>
+    <el-card>
+      <el-table :data="list">
+        <el-table-column align="center" width="200" label="角色编号" prop="id" />
+        <el-table-column align="center" width="200" label="角色名称" prop="name" />
+        <el-table-column align="center" width="200" label="状态">
+          <template v-slot="{ row }">
+            <span> {{ row.state === 1 ? '已启用' : row.state === 0 ? '未启用' : '无' }} </span>
+          </template>
+        </el-table-column>
+        <el-table-column align="center" label="描述" prop="description" show-overflow-tooltip />
+        <el-table-column align="center" label="操作">
+          <template v-slot="{}">
+            <el-link type="primary">修改</el-link>
+            <el-divider direction="vertical"></el-divider>
+            <el-link type="primary">删除</el-link>
+          </template>
+        </el-table-column>
+      </el-table>
 
-    <div class="flex justify-end items-center">
-      <el-pagination layout="prev, pager, next" :total="total" />
-    </div>
+      <div class="flex justify-end items-center mt-16">
+        <el-pagination background layout="total, prev, pager, next, jumper" :total="total" :current-page="pageNo" :page-size="pagesize" />
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -52,6 +60,9 @@ export default {
 
 <style lang="scss" scoped>
 .el-table {
-  margin: 16px auto;
+  // margin: 16px auto;
+}
+
+a {
 }
 </style>
